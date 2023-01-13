92674bc5683d98d5b871e776ebf3305b85b5056e
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-01-13 23:24:05 +0900

    Sidebar 목업 진행 중

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import Head from "next/head";
 import Login from "./login";
-import { Button, Container, Title } from "../styles/home";
+import {
+  Box,
+  ChatBox,
+  ChatRoom,
+  Container,
+  ContentBox,
+  Footer,
+  Header,
+  Left,
+  Main,
+  Right,
+  Strong,
+  Text,
+  TextBox,
+  TextChat,
+  TextEmail,
+  TextId,
+  TextName,
+  UserInfo,
+} from "../styles/home";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../config/firebase";
+import Image from "next/image";
 
 export default function Home() {
   const [user] = useAuthState(auth);
@@ -16,11 +36,77 @@ export default function Home() {
   return (
     <Container priority={true}>
       <Head>
-        <Title>Home page</Title>
+        <title>Home page</title>
       </Head>
 
-      <h1>목업 준비</h1>
-      <Button onClick={signOut}>로그아웃</Button>
+      <ChatBox>
+        <Left>
+          <Header>
+            <TextName>{user.displayName}</TextName>
+            <Image
+              src="https://user-images.githubusercontent.com/69576865/212306697-97d9bea3-1c39-490b-abfe-d40f7118aeb8.svg"
+              alt="logout"
+              width="26px"
+              height="26px"
+              onClick={signOut}
+              style={{ cursor: "pointer" }}
+            />
+          </Header>
+
+          <Main>
+            <Box>
+              <Image
+                src="https://user-images.githubusercontent.com/69576865/212309802-78baa647-6130-4f88-a170-d7b7659ffd96.svg"
+                alt="logo"
+                width="20px"
+                height="20px"
+              />
+              <Text>Text Channels</Text>
+              <Image
+                src="https://user-images.githubusercontent.com/69576865/212322092-25b68461-6f11-441a-9ab5-acfe27b3a83a.svg"
+                alt="logo"
+                width="16px"
+                height="16px"
+                style={{ cursor: "pointer" }}
+                onClick={() => alert("채팅룸을 만드시겠습니까?")}
+              />
+            </Box>
+
+            <ContentBox>
+              <ChatRoom>
+                <Strong>#</Strong>
+                <TextChat>과학자들의 모임</TextChat>
+              </ChatRoom>
+              <ChatRoom>
+                <Strong>#</Strong>
+                <TextChat>철학자들의 모임</TextChat>
+              </ChatRoom>
+              <ChatRoom>
+                <Strong>#</Strong>
+                <TextChat>수학자들의 모임</TextChat>
+              </ChatRoom>
+            </ContentBox>
+          </Main>
+
+          <Footer>
+            <img
+              src="https://user-images.githubusercontent.com/69576865/212329281-6180fd52-4cac-4f52-a3a7-6b66f395c340.svg"
+              alt="logo"
+            />
+            <UserInfo>
+              <TextEmail>{user.email}</TextEmail>
+              <TextBox>
+                <TextId>이동건</TextId>
+                <TextId>#00{user.uid.length}</TextId>
+              </TextBox>
+            </UserInfo>
+          </Footer>
+        </Left>
+
+        <Right>
+          <h1>#general</h1>
+        </Right>
+      </ChatBox>
     </Container>
   );
 }
--- renderer/styles/home.ts
+++ renderer/styles/home.ts
@@ -1,5 +1,121 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
-export const Title = styled.title``;
+
+export const TextName = styled.h1`
+  color: #fff;
+`;
+
+export const Text = styled.h1`
+  color: #96989e;
+  text-transform: uppercase;
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
+
 export const Button = styled.button``;
+
+export const ChatBox = styled.div`
+  display: grid;
+  grid-template-columns: 0.2fr 1fr;
+`;
+
+export const Left = styled.div`
+  background-color: #2f3136;
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  grid-template-rows: 0.1fr 1fr 0.1fr;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.6rem;
+  border-bottom: 1px solid #000;
+  margin-top: 1rem;
+`;
+
+export const Right = styled.div`
+  background-color: #35393f;
+  height: 100vh;
+`;
+
+export const Main = styled.main`
+  padding: 1.6rem;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+export const ContentBox = styled.div`
+  margin: 1.6rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+`;
+
+export const ChatRoom = styled.div`
+  display: flex;
+  color: #96989e;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1rem;
+`;
+
+export const TextChat = styled.p`
+  font-size: 2rem;
+  font-weight: 500;
+`;
+
+export const Strong = styled.h1`
+  font-size: 2.6rem;
+  display: block;
+`;
+
+export const Footer = styled.div`
+  padding: 1.6rem;
+  display: flex;
+  align-items: center;
+  gap: 1.2rem;
+  background-color: #292b2f;
+
+  & img {
+    width: 4.2rem;
+    height: 4.2rem;
+    object-fit: "cover";
+    background-color: #fff;
+    border-radius: 50%;
+    padding: 0.4rem;
+    cursor: pointer;
+  }
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const TextEmail = styled.h1`
+  color: #fff;
+  font-size: 2rem;
+  font-weight: 600;
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const TextId = styled.h1`
+  color: #96989e;
+  font-size: 1.6rem;
+  font-weight: 400;
+`;
