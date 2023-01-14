734e4ae90d1dec9cbf1bab3445fad77c0be2b8ea
이동건 <developer@idong-geon-ui-MacBookPro.local>
2023-01-14 16:08:21 +0900

    목업 기본형태 대부분 구현완료

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -3,15 +3,39 @@ import Head from "next/head";
 import Login from "./login";
 import {
   Box,
+  Button,
   ChatBox,
+  ChatContainer,
+  ChatContentBox,
+  ChatDiv,
+  ChatForm,
+  ChatInfo,
+  ChatName,
   ChatRoom,
+  ChatText,
   Container,
   ContentBox,
+  Contents,
+  DatesBox,
   Footer,
   Header,
+  HeaderBox,
+  HeaderRight,
+  HeaderStrong,
+  HeaderText,
+  Headline,
+  ImgBox,
+  Input,
   Left,
+  Line,
+  Logo,
   Main,
+  MainChat,
+  MainContent,
+  MainText,
   Right,
+  ScrollLine,
+  Small,
   Strong,
   Text,
   TextBox,
@@ -19,6 +43,7 @@ import {
   TextEmail,
   TextId,
   TextName,
+  Title,
   UserInfo,
 } from "../styles/home";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
@@ -55,18 +80,20 @@ export default function Home() {
 
           <Main>
             <Box>
-              <Image
-                src="https://user-images.githubusercontent.com/69576865/212309802-78baa647-6130-4f88-a170-d7b7659ffd96.svg"
-                alt="logo"
-                width="20px"
-                height="20px"
-              />
-              <Text>Text Channels</Text>
+              <Headline>
+                <Image
+                  src="https://user-images.githubusercontent.com/69576865/212309802-78baa647-6130-4f88-a170-d7b7659ffd96.svg"
+                  alt="right-arrow"
+                  width="20px"
+                  height="20px"
+                />
+                <Text>Text Channels</Text>
+              </Headline>
               <Image
                 src="https://user-images.githubusercontent.com/69576865/212322092-25b68461-6f11-441a-9ab5-acfe27b3a83a.svg"
-                alt="logo"
-                width="16px"
-                height="16px"
+                alt="plus-icon"
+                width="18px"
+                height="18px"
                 style={{ cursor: "pointer" }}
                 onClick={() => alert("채팅룸을 만드시겠습니까?")}
               />
@@ -96,7 +123,7 @@ export default function Home() {
             <UserInfo>
               <TextEmail>{user.email}</TextEmail>
               <TextBox>
-                <TextId>이동건</TextId>
+                <TextId>{user.displayName}</TextId>
                 <TextId>#00{user.uid.length}</TextId>
               </TextBox>
             </UserInfo>
@@ -104,7 +131,96 @@ export default function Home() {
         </Left>
 
         <Right>
-          <h1>#general</h1>
+          <HeaderRight>
+            <HeaderBox>
+              <HeaderStrong>#</HeaderStrong>
+              <HeaderText>철학자들의 모임</HeaderText>
+            </HeaderBox>
+
+            <HeaderBox>
+              <Image
+                src="https://user-images.githubusercontent.com/69576865/212446527-175c0289-082a-44a0-a8cd-2fa227e1bcc5.svg"
+                alt="logo"
+                width="28px"
+                height="28px"
+                style={{ cursor: "pointer" }}
+              />
+
+              <Image
+                src="https://user-images.githubusercontent.com/69576865/212446683-1f1b2052-987b-47ba-99d7-3e520f21e2c2.svg"
+                alt="logo"
+                width="28px"
+                height="28px"
+                style={{ cursor: "pointer" }}
+              />
+
+              <Image
+                src="https://user-images.githubusercontent.com/69576865/212446644-cc2b9a38-88d9-4bf0-8a77-66b157e63327.svg"
+                alt="logo"
+                width="28px"
+                height="28px"
+                style={{ cursor: "pointer" }}
+              />
+            </HeaderBox>
+          </HeaderRight>
+
+          <ScrollLine>
+            <MainChat>
+              <MainContent>
+                <ImgBox>
+                  <Logo>#</Logo>
+                </ImgBox>
+                <Title>#철학자들의 모임에 오신 걸 환영합니다!</Title>
+                <MainText>#철학자들의 모임 채널의 시작이에요.</MainText>
+              </MainContent>
+            </MainChat>
+
+            <ChatContainer>
+              <DatesBox>
+                <Line />
+                <Small>2023년 1월 14일</Small>
+                <Line />
+              </DatesBox>
+
+              <ChatContentBox>
+                <Contents>
+                  <img
+                    src="https://user-images.githubusercontent.com/69576865/212329281-6180fd52-4cac-4f52-a3a7-6b66f395c340.svg"
+                    alt="logo"
+                  />
+                  <ChatInfo>
+                    <ChatDiv>
+                      <ChatName>이동건</ChatName>
+                      <Small>오후 3:28</Small>
+                    </ChatDiv>
+                    <ChatText>
+                      안녕하세요. 오늘은 정말 좋은날인거 같아요!
+                    </ChatText>
+                  </ChatInfo>
+                </Contents>
+              </ChatContentBox>
+              <ChatContentBox>
+                <Contents>
+                  <img
+                    src="https://user-images.githubusercontent.com/69576865/212329281-6180fd52-4cac-4f52-a3a7-6b66f395c340.svg"
+                    alt="logo"
+                  />
+                  <ChatInfo>
+                    <ChatDiv>
+                      <ChatName>권은혜</ChatName>
+                      <Small>오후 3:28</Small>
+                    </ChatDiv>
+                    <ChatText>저도 그렇게 생각합니다.</ChatText>
+                  </ChatInfo>
+                </Contents>
+              </ChatContentBox>
+            </ChatContainer>
+          </ScrollLine>
+
+          <ChatForm>
+            <Input type="text" placeholder="철학자들의 모임에 메시지 보내기" />
+            <Button type="submit" hidden></Button>
+          </ChatForm>
         </Right>
       </ChatBox>
     </Container>
--- renderer/styles/home.ts
+++ renderer/styles/home.ts
@@ -9,7 +9,7 @@ export const TextName = styled.h1`
 export const Text = styled.h1`
   color: #96989e;
   text-transform: uppercase;
-  font-size: 1.5rem;
+  font-size: 1.4rem;
   font-weight: 500;
 `;
 
@@ -34,12 +34,11 @@ export const Header = styled.header`
   align-items: center;
   padding: 1.6rem;
   border-bottom: 1px solid #000;
-  margin-top: 1rem;
 `;
 
-export const Right = styled.div`
+export const Right = styled(Left)`
   background-color: #35393f;
-  height: 100vh;
+  grid-template-rows: 0.1fr 1.5fr 0.18fr;
 `;
 
 export const Main = styled.main`
@@ -48,7 +47,14 @@ export const Main = styled.main`
 
 export const Box = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  align-items: center;
+  gap: 4rem;
+  line-height: 2;
+`;
+
+export const Headline = styled.div`
+  display: flex;
   align-items: center;
   gap: 0.4rem;
 `;
@@ -57,7 +63,6 @@ export const ContentBox = styled.div`
   margin: 1.6rem;
   display: flex;
   flex-direction: column;
-  gap: 2.4rem;
 `;
 
 export const ChatRoom = styled.div`
@@ -66,16 +71,23 @@ export const ChatRoom = styled.div`
   align-items: center;
   justify-content: flex-start;
   gap: 1rem;
+  width: 100%;
+
+  &:hover {
+    background-color: #35393f;
+    cursor: pointer;
+  }
 `;
 
 export const TextChat = styled.p`
-  font-size: 2rem;
+  font-size: 1.8rem;
   font-weight: 500;
 `;
 
 export const Strong = styled.h1`
   font-size: 2.6rem;
   display: block;
+  color: #96989e;
 `;
 
 export const Footer = styled.div`
@@ -86,12 +98,12 @@ export const Footer = styled.div`
   background-color: #292b2f;
 
   & img {
-    width: 4.2rem;
-    height: 4.2rem;
-    object-fit: "cover";
+    width: 4rem;
+    height: 4rem;
+    object-fit: cover;
     background-color: #fff;
     border-radius: 50%;
-    padding: 0.4rem;
+    padding: 0.5rem;
     cursor: pointer;
   }
 `;
@@ -118,4 +130,155 @@ export const TextId = styled.h1`
   color: #96989e;
   font-size: 1.6rem;
   font-weight: 400;
+`;
+
+export const ScrollLine = styled.div`
+  overflow-y: scroll;
+`;
+
+export const HeaderRight = styled(Header)``;
+
+export const HeaderBox = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 2.2rem;
+  gap: 1.8rem;
+`;
+
+export const HeaderStrong = styled(Strong)`
+  font-size: 3.3rem;
+`;
+
+export const HeaderText = styled.h1`
+  font-size: 2rem;
+  line-height: 2;
+  color: #fff;
+`;
+
+export const MainChat = styled.main`
+  padding: 1.6rem;
+`;
+
+export const MainContent = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+  margin-top: auto;
+  gap: 1.2rem;
+`;
+
+export const ImgBox = styled.div`
+  background-color: #4f545c;
+  display: inline-block;
+  border-radius: 50%;
+  padding: 1rem 2rem;
+`;
+
+export const Logo = styled.h1`
+  font-size: 6.2rem;
+  line-height: 1;
+  color: #fff;
+`;
+
+export const Title = styled.h1`
+  color: #fff;
+  font-size: 3.6rem;
+  letter-spacing: 0.2px;
+`;
+
+export const MainText = styled.p`
+  color: #96989e;
+  font-size: 1.8rem;
+`;
+
+export const ChatContainer = styled.div`
+  padding: 1.6rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const ChatContentBox = styled.div``;
+
+export const Contents = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+
+  & img {
+    width: 4.2rem;
+    height: 4.2rem;
+    object-fit: cover;
+    background-color: #fff;
+    border-radius: 50%;
+    padding: 0.4rem;
+    cursor: pointer;
+  }
+`;
+
+export const ChatName = styled.h1`
+  font-size: 2rem;
+  color: #fff;
+  width: 100%;
+`;
+
+export const ChatText = styled.p`
+  font-size: 1.8rem;
+  font-weight: 400;
+  color: #dcddde;
+`;
+
+export const DatesBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 0.2fr 1fr;
+  align-items: center;
+`;
+
+export const Line = styled.hr`
+  width: 100%;
+  height: 1px;
+  border: 1px solid #41454e;
+`;
+
+export const Small = styled.small`
+  width: 100%;
+  color: #dcddde;
+  font-size: 1.4rem;
+  text-align: center;
+`;
+
+export const ChatForm = styled.form`
+  padding: 1.6rem;
+`;
+
+export const ChatDiv = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  & h1 {
+    flex: 0.2;
+    text-align: left;
+  }
+
+  & small {
+    flex: 3;
+    text-align: left;
+  }
+`;
+
+export const ChatInfo = styled.div`
+  /* display: flex; */
+  width: 100%;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 1.6rem;
+  background-color: #40444b;
+  color: #dcddde;
+  font-size: 1.8rem;
+  font-weight: 400;
+  border-radius: 1rem;
 `;
